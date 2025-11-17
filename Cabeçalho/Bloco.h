ff10e2e86218291977a81595c5fdf404fc5c8470
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-17 20:50:46 -0300

    Update Bloco.h

--- Cabeçalho/Bloco.h
+++ Cabeçalho/Bloco.h
@@ -5,7 +5,7 @@
 #ifndef BLOCO
 #define BLOCO
 
-void iniciarBloco(Bloco *bloco, float posx, float posy);
+void iniciarBloco(Bloco *bloco, float posx, float posy, int vida);
 
 void desenharBloco(Bloco *bloco);
 
