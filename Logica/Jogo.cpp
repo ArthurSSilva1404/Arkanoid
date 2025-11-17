6fdad43df1f5a3382658b715a13e161018ce28b7
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-17 19:40:09 -0300

    Refactor game properties and clean up comments
    Removed commented-out code and updated game properties.

--- Logica/Jogo.cpp
+++ Logica/Jogo.cpp
@@ -14,21 +14,15 @@
   jogo.bola.velocidadex = 200;
   jogo.bola.velocidadey = -250;
 
-/*
-
   jogo.barra.retangulo.posicao.x = 410;
   jogo.barra.retangulo.posicao.y = 560;
   jogo.barra.retangulo.largura = 160;
   jogo.barra.retangulo.altura = 16;
   jogo.barra.velocidade = 700;
 
-  jogo.fase.blocos
-
-*/
-
-  jogo.niveis = 3;
+  jogo.dificuldade = 3;
   jogo.blocosRestantes = 0;
-  jogo.vidas_jogador = 3;
+  jogo.fase_atual = 3;
 
 }
 
