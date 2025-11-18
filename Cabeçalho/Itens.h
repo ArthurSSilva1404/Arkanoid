4b883c7106a584966ade5792ec8be16f3ec16e5e
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-17 21:28:56 -0300

    Add function declarations for item management

--- Cabeçalho/Itens.h
+++ Cabeçalho/Itens.h
@@ -1,1 +1,15 @@
 
+#include <iostream>
+
+#include "Estruturas.h"
+
+#ifndef ITENS
+#define ITENS
+
+void iniciarItem(ItensEspeciais *item, int tipo, int posx, int posy);
+
+void desenharItem(ItensEspeciais *item);
+
+void atualizarItem(ItensEspeciais *item);
+
+#endif
