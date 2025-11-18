488cdde07a6e671a495054100c64e12eb8e9d1ef
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-17 21:40:13 -0300

    Update Fases.h

--- Cabeçalho/Fases.h
+++ Cabeçalho/Fases.h
@@ -1,1 +1,17 @@
 
+#include <iostream>
+
+#include "Estruturas.h"
+
+#ifndef FASES
+#define FASES
+
+void iniciarFase(Fase *fase, int fase);
+
+void desenharFase(Fase *fase);
+
+void gerarItem(Fase *fase, ItensEspeciais item[], int maxItens);
+
+bool terminarFase(Fase *fase)
+
+#endif
