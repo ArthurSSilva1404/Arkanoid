9e6cfe0ab141875cfa2c911bde71fd22b376bb36
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-24 20:19:44 -0300

    Refactor iniciarBarra and atualizarTamanho functions

--- Logica/Barra.cpp
+++ Logica/Barra.cpp
@@ -1,6 +1,24 @@
-
 #include "Estruturas.h"
 #include "Barra.h"
+
+void iniciarBarra(Barra *barra) {
+
+    barra->retangulo.posicao.x = 350;
+    barra->retangulo.posicao.y = 550;
+    barra->retangulo.largura = 100;
+    barra->retangulo.altura = 20;
+    barra->velocidadex = 0;
+
+}
+
+void desenharBarra(Barra *barra) {
+
+    DrawRectangle((int)barra->retangulo.posicao.x, (int)barra->retangulo.posicao.y,
+                  (int)barra->retangulo.largura, (int)barra->retangulo.altura, WHITE);
+
+    DrawRectangleLines ((int)barra->retangulo.posicao.x, (int)barra->retangulo.posicao.y,
+                  (int)barra->retangulo.largura, (int)barra->retangulo.altura, BLACK);
+}
 
 void atualizarTamanho(Barra *barra, int tamanhoAd, bool adicionar)
 {
@@ -14,25 +32,5 @@ void atualizarTamanho(Barra *barra, int tamanhoAd, bool adicionar)
         barra->retangulo.largura -= tamanhoAd;
 
     } //adicionar verificação se a barra está maior que a tela
-
-}
-
-void desenharBarra(Barra *barra) {
-
-    DrawRectangle((int)barra->retangulo.posicao.x, (int)barra->retangulo.posicao.y,
-                  (int)barra->retangulo.largura, (int)barra->retangulo.altura, WHITE);
-
-    DrawRectangleLines ((int)barra->retangulo.posicao.x, (int)barra->retangulo.posicao.y,
-                  (int)barra->retangulo.largura, (int)barra->retangulo.altura, BLACK);
-
-}
-
-void iniciarBarra(Barra *barra) {
-
-    barra->retangulo.posicao.x = 350;
-    barra->retangulo.posicao.y = 550;
-    barra->retangulo.largura = 100;
-    barra->retangulo.altura = 20;
-    barra->velocidadex = 0;
 
 }
