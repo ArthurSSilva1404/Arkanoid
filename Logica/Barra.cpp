33144f7e26bc37ec819d6721dcc6bda7bac419fd
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-24 20:18:22 -0300

    Implement functions to update and draw Barra

--- Logica/Barra.cpp
+++ Logica/Barra.cpp
@@ -1,1 +1,38 @@
 
+#include "Estruturas.h"
+#include "Barra.h"
+
+void atualizarTamanho(Barra *barra, int tamanhoAd, bool adicionar)
+{
+    
+    if (adicionar) {
+
+        barra->retangulo.largura += tamanhoAd;
+
+    } else {
+
+        barra->retangulo.largura -= tamanhoAd;
+
+    } //adicionar verificação se a barra está maior que a tela
+
+}
+
+void desenharBarra(Barra *barra) {
+
+    DrawRectangle((int)barra->retangulo.posicao.x, (int)barra->retangulo.posicao.y,
+                  (int)barra->retangulo.largura, (int)barra->retangulo.altura, WHITE);
+
+    DrawRectangleLines ((int)barra->retangulo.posicao.x, (int)barra->retangulo.posicao.y,
+                  (int)barra->retangulo.largura, (int)barra->retangulo.altura, BLACK);
+
+}
+
+void iniciarBarra(Barra *barra) {
+
+    barra->retangulo.posicao.x = 350;
+    barra->retangulo.posicao.y = 550;
+    barra->retangulo.largura = 100;
+    barra->retangulo.altura = 20;
+    barra->velocidadex = 0;
+
+}
