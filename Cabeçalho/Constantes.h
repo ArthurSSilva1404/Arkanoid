82cb63996a80b446bf69e4db95413cef70ef5976
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-24 21:27:12 -0300

    Update Constantes.h

--- Cabeçalho/Constantes.h
+++ Cabeçalho/Constantes.h
@@ -4,6 +4,23 @@
 #define CONSTANTES
 
 #define MAX_JOGADORES 10
-#define MAX_LETRAS 10
+#define MAX_LETRAS 50
+#define LARGURA_TELA 800
+#define ALTURA_TELA 600
+#define TAMANHO_BLOCO_LARGURA 60
+#define TAMANHO_BLOCO_ALTURA 20
+#define RAIO_BOLA 5
+#define VELOCIDADE_BOLA 300
+#define VELOCIDADE_BARRA 400
+#define VIDAS_INICIAIS 3
+#define MAX_BLOCOS 100
+#define ARQUIVO_RANKING "ranking.txt"
+
+// Tipos de itens especiais
+#define ITEM_VIDA 1
+#define ITEM_AUMENTAR_BARRA 2
+#define ITEM_DIMINUIR_BARRA 3
+#define ITEM_PONTOS_EXTRAS 4
+#define ITEM_INVERSOR 5
 
 #endif
