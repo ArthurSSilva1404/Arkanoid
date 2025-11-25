513bd100dc0d9b98758c022567622bd2118fe7d8
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-24 21:34:10 -0300

    Update Jogador.cpp

--- Logica/Jogador.cpp
+++ Logica/Jogador.cpp
@@ -1,1 +1,18 @@
+#include "Estruturas.h"
+#include "Jogador.h"
+#include "Constantes.h"
 
+void atualizarPontuacao(Jogador *jogador, int pontuacao) {
+    jogador->pontuacao += pontuacao;
+    if (jogador->pontuacao < 0) {
+        jogador->pontuacao = 0;
+    }
+}
+
+void atualizarTempo(Jogador *jogador) {
+    jogador->tempo++;
+}
+
+void atualizarVidas(Jogador *jogador) {
+    jogador->vidas_jogador--;
+}
