974197031b64cae9e5971fe564cad8f59c7b4a31
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-24 21:35:15 -0300

    Update and rename cores.cpp to Cores.cpp

---
+++ Logica/Cores.cpp
@@ -0,0 +1,55 @@
+#include "raylib.h"
+
+// Paleta de cores do jogo
+Color obterCorBlocoForte() {
+    return RED;
+}
+
+Color obterCorBlocoMedio() {
+    return ORANGE;
+}
+
+Color obterCorBlocoFraco() {
+    return YELLOW;
+}
+
+Color obterCorBola() {
+    return YELLOW;
+}
+
+Color obterCorBarra() {
+    return WHITE;
+}
+
+Color obterCorItemVida() {
+    return GREEN;
+}
+
+Color obterCorItemAumentar() {
+    return BLUE;
+}
+
+Color obterCorItemDiminuir() {
+    return PURPLE;
+}
+
+Color obterCorItemPontos() {
+    return GOLD;
+}
+
+Color obterCorItemInversor() {
+    return PINK;
+}
+
+Color obterCorFundo() {
+    return DARKBLUE;
+}
+
+Color obterCorTexto() {
+    return WHITE;
+}
+
+Color obterCorTitulo() {
+    return YELLOW;
+}
+
