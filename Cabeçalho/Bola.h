3b2e5afd7d639f37bf273fc23fc959ebd2fdf2ac
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-26 21:58:53 -0300

    Update Bola.h

--- Cabeçalho/Bola.h
+++ Cabeçalho/Bola.h
@@ -1,16 +1,20 @@
-#include <iostream>
-
 #include "Estruturas.h"
 
 #ifndef BOLA
 #define BOLA
 
+struct Barra;
+
 void iniciarBola(Bola *bola);
 
-void atualizarBola(Bola *bola, int dt);
+void atualizarBola(Bola *bola, float dt);
 
 void atualizarTamanho(Bola *bola, int tamanhoAd);
 
 void desenharBola(Bola *bola);
+
+void alinharBolaNaBarra(Bola *bola, const Barra *barra);
+
+void soltarBola(Bola *bola, float fatorVelocidade);
 
 #endif
