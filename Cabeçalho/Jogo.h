99497ffaaa01477590dced643317fd9821934b91
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-26 22:01:50 -0300

    Update Jogo.h

--- Cabeçalho/Jogo.h
+++ Cabeçalho/Jogo.h
@@ -15,12 +15,31 @@ struct EstadoJogo {
     Barra barra;
     Fase fase;
     ItensEspeciais itens[MAX_ITENS];
+    SistemaParticulas sistemaParticulas;
+    EstatisticasPartida estatisticas;
     Jogador jogador;
     int dificuldade;
     int qtd_itens;
     int fase_atual;
-   // Font fonte;
-
+    bool aguardandoLancamento;
+    bool jogoConcluido;
+    float tempoFase;
+    float multiplicadorVelocidade;
+    bool escudoAtivo;
+    float tempoEscudoRestante;
+    Rectangle escudoArea;
+    Meteoro meteoros[MAX_METEOROS];
+    float tempoParaProximoMeteoro;
 };
+
+void prepararEstadoJogo(EstadoJogo *estado, const char *nome, int dificuldade);
+
+void reiniciarElementosDaFase(EstadoJogo *estado);
+
+float obterMultiplicadorDificuldade(int dificuldade);
+
+const char *obterNomeDificuldade(int dificuldade);
+
+void consolidarPontuacaoFinal(EstadoJogo *estado);
 
 #endif
