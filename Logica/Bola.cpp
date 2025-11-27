ad4cf21c49679f68ceb76b712f06a4645fb68968
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-26 22:04:00 -0300

    Update Bola.cpp

--- Logica/Bola.cpp
+++ Logica/Bola.cpp
@@ -1,20 +1,34 @@
 #include "Estruturas.h"
 #include "Bola.h"
 #include "Constantes.h"
-#include "cores.h"
+#include "Cores.h"
+#include "Barra.h"
 #include "raylib.h"
 #include <cmath>
 
 void iniciarBola(Bola *bola) {
     bola->posicao.x = LARGURA_TELA / 2.0f;
-    bola->posicao.y = ALTURA_TELA - 50;
+    bola->posicao.y = ALTURA_TELA - 60;
     bola->radium = RAIO_BOLA;
-    bola->velocidadex = 200.0f;
-    bola->velocidadey = -250.0f;
+    bola->velocidadex = 0.0f;
+    bola->velocidadey = -VELOCIDADE_BOLA_BASE;
+    bola->velocidadeBase = VELOCIDADE_BOLA_BASE;
     bola->prendeu = true;
 }
 
-void atualizarBola(Bola *bola, int dt) {
+void alinharBolaNaBarra(Bola *bola, const Barra *barra) {
+    float centroBarra = barra->retangulo.posicao.x + barra->retangulo.largura / 2.0f;
+    bola->posicao.x = centroBarra;
+    bola->posicao.y = barra->retangulo.posicao.y - bola->radium - 4.0f;
+}
+
+void soltarBola(Bola *bola, float fatorVelocidade) {
+    bola->prendeu = false;
+    bola->velocidadey = -bola->velocidadeBase * fatorVelocidade;
+    bola->velocidadex = bola->velocidadeBase * fatorVelocidade * 0.35f;
+}
+
+void atualizarBola(Bola *bola, float dt) {
     if (!bola->prendeu) {
         bola->posicao.x += bola->velocidadex * dt;
         bola->posicao.y += bola->velocidadey * dt;
@@ -48,4 +62,3 @@ void desenharBola(Bola *bola) {
     DrawCircle((int)bola->posicao.x, (int)bola->posicao.y, bola->radium, obterCorBola());
     DrawCircleLines((int)bola->posicao.x, (int)bola->posicao.y, bola->radium, ORANGE);
 }
-
