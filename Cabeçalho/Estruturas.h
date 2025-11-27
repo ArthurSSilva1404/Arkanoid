1833c5de9de64bc492daaafd443791b7719c558f
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-26 22:00:07 -0300

    Update Estruturas.h

--- Cabeçalho/Estruturas.h
+++ Cabeçalho/Estruturas.h
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "Constantes.h"
+#include "raylib.h"
 
 #ifndef ESTRUTURAS_H
 #define ESTRUTURAS_H
@@ -20,6 +21,11 @@ struct Bloco {
     Retangulo2D retangulo;
     bool ativo;
     int TotalDeVida;
+    Color cor;
+    int pontosBase;
+    bool possuiItem;
+    bool indestrutivel;
+    int tipo;
 };
 
 struct Bola {
@@ -27,6 +33,7 @@ struct Bola {
     float radium;
     float velocidadex;
     float velocidadey;
+    float velocidadeBase;
     bool prendeu;
 };
 
@@ -39,7 +46,10 @@ struct Fase {
     Bloco *blocos;
     int linha;
     int coluna;
-    
+    int indiceMapa;
+    char nome[32];
+    Color corFundo;
+    Color corDetalhe;
 };
 
 struct ItensEspeciais {
@@ -47,6 +57,30 @@ struct ItensEspeciais {
     int tipo;
     float velocidadey;
     bool esta_Ativo;
+    float tamanho;
+    Color cor;
+};
+
+struct EstatisticasPartida {
+    int blocosQuebrados;
+    int itensColetados;
+    int bonusPontuacao;
+    int vidasGastas;
+    int fasesConcluidas;
+    bool pontuacaoInvertida;
+};
+
+struct Particula {
+    Vector2 posicao;
+    Vector2 velocidade;
+    float tempoVida;
+    float tempoMaximo;
+    Color cor;
+    bool ativa;
+};
+
+struct SistemaParticulas {
+    Particula particulas[MAX_PARTICULAS];
 };
 
 struct Jogador {
@@ -54,7 +88,18 @@ struct Jogador {
     int pontuacao;
     int tempo;
     int vidas_jogador;
+    char dataRegistro[20];
+    int dificuldadeSelecionada;
+    int itens_coletados;
+    int blocos_quebrados;
+};
 
+struct Meteoro {
+    Vector2 posicao;
+    Vector2 velocidade;
+    float tamanho;
+    Color cor;
+    bool ativo;
 };
 
 struct Ranking {
