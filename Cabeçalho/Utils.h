11dfe8911f2acf22a72a90cc350e81bedf5d5683
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-26 22:02:38 -0300

    Update Utils.h

--- Cabeçalho/Utils.h
+++ Cabeçalho/Utils.h
@@ -5,9 +5,8 @@
 
 bool verificarColisaoCirculoRetangulo(Posicao circulo, float raio, Retangulo2D retangulo);
 
-int calcularPontuacao(int tempo, int dificuldade, int blocos_quebrados);
+int calcularPontuacao(int tempo, int dificuldade, int blocos_quebrados, int itens_coletados, int vidas_restantes);
 
 int gerarAleatorio(int min, int max);
 
 #endif
-
