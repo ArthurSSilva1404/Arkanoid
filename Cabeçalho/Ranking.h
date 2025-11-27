d7fbdee82b2f3ddddd10808c182891c6df087692
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-26 22:02:16 -0300

    Update Ranking.h

--- Cabeçalho/Ranking.h
+++ Cabeçalho/Ranking.h
@@ -1,17 +1,14 @@
+#ifndef RANKING_H
+#define RANKING_H
+
 #include "raylib.h"
-
 #include <iostream>
-
 #include "Estruturas.h"
 
 void iniciarRanking(Ranking *ranking, char *arquivo);
-
 void atualizarRanking(Ranking *ranking, Jogador *jogador);
-
 void desenharRanking(Ranking *ranking);
-
 void salvarRanking(Ranking *ranking);
-
 void carregarRanking(Ranking *ranking);
 
 #endif
