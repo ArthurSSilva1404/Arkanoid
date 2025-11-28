8a0efb5ab3d3b80b3b0b29df085505b2d7028cfc
ArthurSSilva1404 <arthurssilva1404@gmail.com>
2025-11-27 22:55:16 -0300

    Implementar funções de Jogador (atualizarPontuacao, atualizarTempo, atualizarVidas)

--- Logica/Jogador.cpp
+++ Logica/Jogador.cpp
@@ -1,14 +1,21 @@
+#include "../Cabeçalho/Jogador.h"
+#include "../Cabeçalho/Estruturas.h"
 #include <iostream>
 
-#include "Estruturas.h"
+void atualizarPontuacao(Jogador *jogador, int pontuacao) {
+    if (jogador != nullptr) {
+        jogador->pontuacao += pontuacao;
+    }
+}
 
-#ifndef JOGADOR
-#define JOGADOR
+void atualizarTempo(Jogador *jogador) {
+    if (jogador != nullptr) {
+        jogador->tempo++;
+    }
+}
 
-void atualizarPontuacao(Jogador *jogador, int pontuacao);
-
-void atualizarTempo(Jogador *jogador);
-
-void atualizarVidas(Jogador *jogador);
-
-#endif
+void atualizarVidas(Jogador *jogador) {
+    if (jogador != nullptr) {
+        jogador->vidas_jogador--;
+    }
+}
